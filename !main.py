680ad44c424cdbc97be5b2060d3afcc77668b433
Amsterwolf <2497695115@qq.com>
2021-01-23 22:44:25 +0800

    1/23

--- !main.py
+++ !main.py
@@ -1,5 +1,21 @@
 import matplotlib.pyplot as pyplot
-sqs=[1,4,16,25]
-fig,ax=pyplot.subplots()
-ax.plot(sqs)
+
+x=range(1,1001)
+y=[i**2 for i in x]
+pyplot.style.use("seaborn")#set style(before...)
+fig,ax=pyplot.subplots()#传递地址
+
+
+
+ax.set_title("square",fontsize=24)
+ax.set_xlabel("val",fontsize=24)
+ax.set_ylabel("val*val",fontsize=24)
+ax.tick_params(axis="both",labelsize=8)
+
+ax.plot(x, y,linewidth=1)#线段绘制
+#ax.scatter(x,y,s=10)#描点绘制
+ax.axis([0,1100,0,1100000])
+
+print(pyplot.style.available)
+
 pyplot.show()
---
+++ plot_single_point.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as pyplot
+
+pyplot.style.use("seaborn")
+fig,ax=pyplot.subplots()
+ax.scatter(2,4,s=2000)
+
+ax.set_title("point fig",fontsize=22)
+ax.set_xlabel("x",fontsize=22)
+ax.set_ylabel("y",fontsize=22)
+
+ax.tick_params(axis="both",which='major',labelsize=10)
+
+
+pyplot.show()
